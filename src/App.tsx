199e8732dc8d5e0fa9a5ceca81ae4f64cc198462
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-09-29 23:18:21 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>hello world</h1>
+        <Button>My button</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;

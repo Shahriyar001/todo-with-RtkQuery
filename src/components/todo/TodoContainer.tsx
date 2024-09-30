2d85797cbb97ca502c864063fc472c174a1403c3
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-09-30 22:56:29 +0600

    Todo component's basic structure

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,7 +1,21 @@
+import TodoCard from "./TodoCard";
+
 const TodoContainer = () => {
   return (
     <div>
-      <h1>container</h1>
+      <div>
+        <button>Add Todo</button>
+        <button>Filter</button>
+      </div>
+      <div className="bg-red-500 w-full h-full rounded-xl p-5 space-y-3">
+        {/* <div className="bg-white text-2xl font-blod p-5 flex justify-center items-center rounded-md">
+          <p>There is no task pending</p>
+        </div> */}
+        <TodoCard />
+        <TodoCard />
+        <TodoCard />
+        <TodoCard />
+      </div>
     </div>
   );
 };
--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,11 +1,12 @@
 import TodoContainer from "@/components/todo/TodoContainer";
+import Container from "@/components/ui/Container";
 
 const Todo = () => {
   return (
-    <div>
-      <h1>My Todos</h1>
+    <Container>
+      <h1 className="text-center text-3xl font-semibold my-10">My Todos</h1>
       <TodoContainer />
-    </div>
+    </Container>
   );
 };
 

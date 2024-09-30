52d7b545399737919d8bade1d6372030fec1dac4
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-09-30 23:45:01 +0600

    added some component of shadcd

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,20 +1,24 @@
+import { Button } from "../ui/button";
 import TodoCard from "./TodoCard";
 
 const TodoContainer = () => {
   return (
     <div>
-      <div>
-        <button>Add Todo</button>
-        <button>Filter</button>
+      <div className="flex justify-between mb-5">
+        <Button className="bg-blue-500 text-xl font-semibold">Add Todo</Button>
+        <Button className="bg-blue-500">Filter</Button>
       </div>
-      <div className="bg-red-500 w-full h-full rounded-xl p-5 space-y-3">
-        {/* <div className="bg-white text-2xl font-blod p-5 flex justify-center items-center rounded-md">
+      <div className="bg-blue-600 w-full h-full rounded-xl p-[5px]">
+        <div className="bg-white p-5 w-full h-full rounded-lg  space-y-3">
+          {/* <div className="bg-white text-2xl font-blod p-5 flex justify-center items-center rounded-md">
           <p>There is no task pending</p>
-        </div> */}
-        <TodoCard />
-        <TodoCard />
-        <TodoCard />
-        <TodoCard />
+          </div> */}
+
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+        </div>
       </div>
     </div>
   );
